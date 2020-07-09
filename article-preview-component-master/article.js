3e18802877b4ca47677c4c810a5074296d113c2d
rishita2605 <64982040+rishita2605@users.noreply.github.com>
2020-07-09 22:08:30 +0530

    22:08

--- article-preview-component-master/article.js
+++ article-preview-component-master/article.js
@@ -1,1 +1,10 @@
 //variables
+const button=document.querySelector(".btn");
+
+//event listeners
+button.addEventListener("click",changeStyle);
+//functions
+
+function changeStyle(){
+    console.log("abc");
+}
