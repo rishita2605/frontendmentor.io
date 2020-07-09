e9b44831471848f55d8e719862d9e17506df8212
rishita2605 <64982040+rishita2605@users.noreply.github.com>
2020-07-10 00:29:41 +0530

    00:29

--- article-preview-component-master/article.js
+++ article-preview-component-master/article.js
@@ -1,6 +1,7 @@
 //variables
 const button=document.querySelector(".btn");
 const image=document.querySelector(".share-img");
+const afterS=document.querySelector(".share-after");
 button.style.backgroundColor="rgb(236, 242, 249)";
 //event listeners
 button.addEventListener("click",changeStyle);
@@ -29,7 +30,10 @@ function changeStyleTwo(){
     button.style.transform="scale(1)";
     console.log(image.src);
     image.src="./images/share-after.png";
-    button.style.backgroundColor="rgb(73, 85, 107)";
+    button.style.backgroundColor="rgb(108, 127, 153)";
+    afterS.style.transform="scaleY(1)";
+    afterS.style.opacity="1";
+
 
 }
 
@@ -37,4 +41,6 @@ function changeStyleOne(){
     button.style.transform="scale(1)";
     image.src="./images/share-before.png";
     button.style.backgroundColor="rgb(236, 242, 249)";
+    afterS.style.transform="scaleY(0)";
+    afterS.style.opacity="0";
 }
